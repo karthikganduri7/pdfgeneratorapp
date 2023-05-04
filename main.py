1bc2f3d6bb78ceeea69855e29f49f44627b3147e
karthikganduri <karthikganduri@yahoo.in>
2023-05-04 16:04:54 +0530

    added lines

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="B", size=24)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-        pdf.line(10, 20, 200, 20)
+        for i2 in range(1, 28):
+            x = i2 * 10
+            y = x + 10
+            pdf.line(10, y, 200, y)
+
         # set footers
         pdf.ln(265)
         pdf.set_font(family="Times", style="I", size=8)

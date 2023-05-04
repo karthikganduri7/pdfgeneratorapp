37dca5ea89bae37e99695f17550ec9780bfca176
karthikganduri <karthikganduri@yahoo.in>
2023-05-04 15:49:41 +0530

    added foot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fpdf import FPDF
 import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     for i in range(row["Pages"]):
@@ -10,6 +10,10 @@ for index, row in df.iterrows():
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
         pdf.line(10, 20, 200, 20)
-
+        # set footers
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
 pdf.output("output.pdf")
 
